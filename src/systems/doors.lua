8e7089b8abddd88bd27b9ab66b42c71b7cb6b6a9
matthurtado <matthewrosshurtado@gmail.com>
2025-08-22 17:49:00 -0400

    Added spritesheet png for easier editing, added new house with door teleports

--- src/systems/doors.lua
+++ src/systems/doors.lua
@@ -6,7 +6,8 @@ STATE.doors.by_cell = STATE.doors.by_cell or {}
 STATE.doors.by_tile = STATE.doors.by_tile or {}
 
 -- Register your door tile(s) here:
-STATE.doors.by_tile[113] = {tx=96, ty=20}  -- door sprite #113 -> interior (96,20)
+STATE.doors.by_tile[113] = {tx=125, ty=005}  -- door sprite #113 -> interior (96,20)
+STATE.doors.by_tile[121] = {tx=008, ty=005}
 
 -- examples for more doors:
 -- STATE.doors.by_tile[114] = {tx=104, ty=18}
